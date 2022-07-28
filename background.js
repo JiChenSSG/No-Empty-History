27fdb3ed4e3eb644887c3ea957f16cc0d710dc21
jiChen <1286856721@qq.com>
2022-07-28 20:00:46 +0800

    完成监听删除

--- background.js
+++ background.js
@@ -1,23 +1,11 @@
-var historys = []
-
-// chrome.history.search({ text: 'pornhub' }, function (historyItems) {
-//     for (var i = 0; i < historyItems.length; i++) {
-//         historys.push(historyItems[i].url)
-//         chrome.history.deleteUrl({ url: historyItems[i].url });
-//     }
-// })
-
+//监听session变化
 chrome.sessions.onChanged.addListener(
-    {
-        function() {
-            chrome.history.search({ text: 'pornhub' }, function (historyItems) {
-                for (var i = 0; i < historyItems.length; i++) {
-                    historys.push(historyItems[i].url)
-                    chrome.history.deleteUrl({ url: historyItems[i].url });
-                }
-            })
-        }
+    function () {
+        chrome.history.search({ text: 'pornhub' }, function (historyItems) {
+            for (var i = 0; i < historyItems.length; i++) {
+                chrome.history.deleteUrl({ url: historyItems[i].url });
+            }
+        })
     }
 )
 
-console.log(historys)
