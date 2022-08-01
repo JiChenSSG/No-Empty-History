da10f4217ef161da2bb7abbda0090559cfeced8b
jiChen <1286856721@qq.com>
2022-08-01 11:24:41 +0800

    修改界面

---
+++ content.js
@@ -0,0 +1,6 @@
+var add = document.getElementById("add");
+var edit = document.getElementById("edit");
+
+add.addEventListener("click", function () {
+    window.location.href = "./add.html";
+});        
